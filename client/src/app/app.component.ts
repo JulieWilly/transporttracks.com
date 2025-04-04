bbfe8119c4bb31307fad2576f5e174c62292b670
Wilfred Muchire <logincoin40@gmail.com>
2025-04-04 08:04:34 +0300

    adding files to the project structure

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+  isServerRunning = true;
+  operatingSystems = [{id: 'win', name: 'Windows'}, {id: 'osx', name: 'MacOS'}, {id: 'linux', name: 'Linux'}];
+  isEditable = true;
+
+  onSubmit() {
+    alert('Documents submitted successfully.');
+  }
 }
+
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './core/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './core/auth/sign-up/sign-up.component';
+import { ServicesComponent } from './features/feature-2/services/services.component';
+import { ProjectsComponent } from './features/feature-2/projects/projects.component';
+import { ContactUsComponent } from './features/feature-2/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component:AppComponent
+    },
+    {
+        path:"sign-in",
+        component:SignInComponent
+    },
+    {
+        path:"sign-up",
+        component:SignUpComponent
+    },
+    {
+        path:"services",
+        component:ServicesComponent
+    },{
+        path:"projects",
+        component:ProjectsComponent
+    },{
+        path:'contact-us',
+        component:ContactUsComponent
+    }
+];
