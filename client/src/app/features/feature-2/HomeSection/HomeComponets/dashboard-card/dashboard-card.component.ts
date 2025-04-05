dc3458594584121fb3ecd3a81a00f69f24b20354
Wilfred Muchire <logincoin40@gmail.com>
2025-04-05 13:14:24 +0300

    added the dashboard cards and data

---
+++ client/src/app/features/feature-2/HomeSection/HomeComponets/dashboard-card/dashboard-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-card',
+  imports: [],
+  templateUrl: './dashboard-card.component.html',
+  styleUrl: './dashboard-card.component.css',
+})
+export class DashboardCardComponent {
+  @Input() amount: number = 0;
+  @Input() title: string = '';
+}
