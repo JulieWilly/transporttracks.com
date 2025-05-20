ca6118f2261d23a7f0d885425618c5ca3f7f6804
Wilfred Muchire <logincoin40@gmail.com>
2025-05-20 21:07:42 +0300

    added new charts on the dashboard

--- client/src/app/components/charts/dashboard-line-chart/dashboard-line-chart.component.ts
+++ client/src/app/components/charts/dashboard-line-chart/dashboard-line-chart.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {
   provideCharts,
   withDefaultRegisterables,
   BaseChartDirective,
 } from 'ng2-charts';
-import { ChartConfiguration } from 'chart.js';
-
+import { ChartConfiguration, ChartData, ChartEvent
+} from 'chart.js';
+import { MatButton } from '@angular/material/button';
 @Component({
   selector: 'app-dashboard-line-chart',
-  imports: [BaseChartDirective],
+  imports: [BaseChartDirective, MatButton, BaseChartDirective],
   providers: [provideCharts(withDefaultRegisterables())],
+  standalone: true,
   templateUrl: './dashboard-line-chart.component.html',
   styleUrl: './dashboard-line-chart.component.css',
 })
 export class DashboardLineChartComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective<'bar'> | undefined;
   // Chart data configuration
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
@@ -33,4 +36,54 @@ export class DashboardLineChartComponent {
     responsive: true,
     maintainAspectRatio: false,
   };
+
+  // bar chart.
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      // datalabels: {
+      //   anchor: 'end',
+      //   align: 'end',
+      // },
+    },
+  };
+
+  public barChartType = 'bar' as const;
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
+  };
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: object[];
+  }): void {
+    console.log(event, active);
+  }
 }
