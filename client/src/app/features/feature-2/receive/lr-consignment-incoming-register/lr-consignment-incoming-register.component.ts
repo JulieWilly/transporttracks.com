f290992765bdce1b085e0b0cfa534cd91ec073fb
Wilfred Muchire <logincoin40@gmail.com>
2025-06-08 11:55:05 +0300

    added design for booking report section

--- client/src/app/features/feature-2/receive/lr-consignment-incoming-register/lr-consignment-incoming-register.component.ts
+++ client/src/app/features/feature-2/receive/lr-consignment-incoming-register/lr-consignment-incoming-register.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { QuickNavigationComponent } from '../../../ReuseableComponents/quick-navigation/quick-navigation.component';
+import { SharableTitleComponent } from '../../../ReuseableComponents/sharable-title/sharable-title.component';
+import { DropdownBtnComponent } from '../../../ReuseableComponents/dropdown-btn/dropdown-btn.component';
+import { BackComponent } from '../../../ReuseableComponents/back/back.component';
+import { DateFromComponent } from '../../../ReuseableComponents/date-from/date-from.component';
+import { DateToComponent } from '../../../ReuseableComponents/date-to/date-to.component';
+import { SearchByComponent } from '../../../ReuseableComponents/search-by/search-by.component';
 
 @Component({
   selector: 'app-lr-consignment-incoming-register',
-  imports: [],
+  imports: [
+    QuickNavigationComponent,
+    SharableTitleComponent,
+    DropdownBtnComponent,
+    BackComponent,
+    DateToComponent,
+    DateFromComponent,
+    SearchByComponent,
+  ],
   templateUrl: './lr-consignment-incoming-register.component.html',
-  styleUrl: './lr-consignment-incoming-register.component.css'
+  styleUrl: './lr-consignment-incoming-register.component.css',
 })
 export class LrConsignmentIncomingRegisterComponent {
-
+  currentPage: string = 'Lr Consignment Incoming Register';
 }
